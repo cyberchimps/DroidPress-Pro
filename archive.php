ea842a73e4c7188a63b9ab9ac2a553b6ba7bc997
CyberChimps <cyberchimpspro@gmail.com>
2011-08-29 11:26:49 -0700

    archive styles

--- archive.php
+++ archive.php
@@ -26,25 +26,25 @@
  			<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
 
 			<?php /* If this is a category archive */ if (is_category()) { ?>
-				<h2><font size="5">Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category:</font></h2><br />
+				<div class="archive-title">Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category:</div><br />
 
 			<?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-				<h2><font size="5">Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;:</font></h2><br />
+				<div class="archive-title">Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;:</div><br />
 
 			<?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-				<h2><font size="5">Archive for <?php the_time('F jS, Y'); ?>:</font></h2><br />
+				<div class="archive-title">Archive for <?php the_time('F jS, Y'); ?>:</div><br />
 
 			<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-				<h2><font size="5">Archive for <?php the_time('F, Y'); ?>:</font></h2><br />
+				<div class="archive-title">Archive for <?php the_time('F, Y'); ?>:</div><br />
 
 			<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-				<h2 class="pagetitle"><font size="5">Archive for <?php the_time('Y'); ?>:</font></h2><br />
+				<div class="archive-title">Archive for <?php the_time('Y'); ?>:</div><br />
 
 			<?php /* If this is an author archive */ } elseif (is_author()) { ?>
-				<h2 class="pagetitle"><font size="5">Author Archive:</font></h2><br />
+				<div class="archive-title"><Author Archive:</div><br />
 
 			<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-				<h2 class="pagetitle"><font size="5">Blog Archives:</font></h2><br />
+				<div class="archive-title">Blog Archives:</div><br />
 
 			
 			<?php } ?>
@@ -55,7 +55,7 @@
 
 				<div <?php post_class() ?>>
 				
-						<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+						<h2 class="archive_posts_title" id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 					
 						<?php get_template_part('meta', 'archive'); ?>
 
@@ -68,10 +68,7 @@
 
 							<div class="postmetadata">
 										<?php get_template_part('share', 'index' ); ?>
-								<div class="comments">
-									<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
-								</div><!--end comments-->	
-							</div><!--end postmetadata-->
+															</div><!--end postmetadata-->
 							
 				</div><!--end post-->
 			</div><!--end post_container-->
